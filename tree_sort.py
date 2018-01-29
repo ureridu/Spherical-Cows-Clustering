5ca3f4e0724fd105bcb5855b4e115a221007428b
ureridu <33601992+ureridu@users.noreply.github.com>
2018-01-29 05:07:08 -0500

    Added Tree Sort Proto

---
+++ tree_sort.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 03:02:57 2018
+
+@author: Ureridu
+"""
+
+l = [5, 3, 8, 7, 0, 0, 1, 2]
+
+class BST:
+    def __init__():
+        self.tree = {}
+        
+    
+    def add_node(node):
+        pass
+    
+    def remove_node(node):
+        pass
